b51740a2094880c674d6de758f46de1c4243261e
PrarthanAgarwal <prarthanagawraljeen@gmail.com>
2024-09-16 08:14:00 +0530

    actual data

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function updateTime() {
     const now = new Date();
-    const timeString = now.toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata' });
+    const timeString = now.toLocaleTimeString('en-IN', {
+        timeZone: 'Asia/Kolkata',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+    });
     document.getElementById('local-time').textContent = timeString;
 }
 
